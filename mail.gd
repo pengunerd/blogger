ad490a3c98af4d8e9ea5bc064a24acb88fee0975
Louis <pengunerd2@gmail.com>
2017-05-11 23:16:40 -0400

    Mail animation!

---
+++ mail.gd
@@ -0,0 +1,20 @@
+
+extends Area2D
+
+# Member variables
+var taken = false
+
+
+func _on_body_enter( body ):
+	if (not taken and body extends preload("res://playerChibi.gd")):
+		get_node("anim").play("taken-mail")
+		taken = true
+		print("MAIL taken")
+
+
+func _on_coin_area_enter(area):
+	pass # replace with function body
+
+
+func _on_coin_area_enter_shape(area_id, area, area_shape, area_shape):
+	pass # replace with function body
