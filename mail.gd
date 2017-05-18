925267e8b9dd114a3c3ac11638b52ac8f256566a
Louis <pengunerd2@gmail.com>
2017-05-17 21:28:32 -0400

    Game timer and scoring!

--- mail.gd
+++ mail.gd
@@ -3,13 +3,15 @@ extends Area2D
 
 # Member variables
 var taken = false
-
+#var mail_score=0
 
 func _on_body_enter( body ):
 	if (not taken and body extends preload("res://playerChibi.gd")):
 		get_node("anim").play("taken-mail")
 		taken = true
 		print("MAIL taken")
+		get_node("/root/game_state").mail_score+=1
+		#print(mail_score)
 
 
 func _on_coin_area_enter(area):
